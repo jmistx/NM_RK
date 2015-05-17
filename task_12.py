68858c0c4e09e3fc4f771ec4002c8c53c26aa0d9
jmistx <blohin.o.d@gmail.com>
2015-05-17 13:29:51 +0300

    Add plots

--- task_12.py
+++ task_12.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import matplotlib
 
 import itertools
@@ -5,6 +7,14 @@ import itertools
 import array
 
 import math
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+font = {'family': 'Verdana',
+        'weight': 'normal'}
+rc('font', **font)
 
 from pprint import pprint
 
@@ -26,6 +36,93 @@ def v_diff(v1, v2):
 def rk2(func, xn, vn, hn): 
 	return v_summ(vn, v_mult(hn/2.0, v_summ(v_func(func, xn, vn), v_func(func, xn + hn, v_summ(vn, v_mult(hn, v_func(func, xn, vn)))))))
 
+def show_S_plot(history):
+	f = plt.figure(1)
+	iters = [h['n'] for h in history]
+
+	plt.subplot(2, 1, 1)
+	plt.title(u'Оценка локальной погрешности')
+	plt.xlabel('i')
+	plt.ylabel('Smax')
+	plt.plot(iters, [h['Smax'] for h in history])
+
+	plt.subplot(2, 1, 2)
+	plt.title(u'Размер шага')
+	plt.xlabel('n')
+	plt.ylabel('h')
+	plt.plot(iters, [h['h'] for h in history])
+	f.show()
+
+def show_Func_plot(result, captions):
+	g = plt.figure(2)
+	_, v = result[0]
+	plotCount = len(v)
+
+	x_result = [x for x, v in result]
+	
+	for i in xrange(plotCount):
+		plt.subplot(plotCount, 1, i + 1)
+
+		if i == 0:
+			plt.title(u'Значения функций')
+
+		plt.ylabel(captions[i])
+
+		if i == plotCount - 1:
+			plt.xlabel(u'x, время')
+
+		
+		plt.plot(x_result, [v[i] for x, v in result])
+
+	g.show()
+
+def show_Fase_portrait(result, captions, members):
+	g = plt.figure(3)
+	x_index, y_index = members
+
+	x_caption = captions[x_index]
+	y_caption = captions[y_index]
+
+	x_axis = [v[x_index] for x, v in result]
+	y_axis = [v[y_index] for x, v in result]
+
+	plt.xlabel(x_caption)
+	plt.ylabel(y_caption)
+
+	plt.title(u'Фазовый портрет: ' + x_caption + ' ' + y_caption)
+	plt.plot(x_axis, y_axis)
+
+	g.show()
+
+def show_exact_and_test_solution(result, captions, solution):
+	g = plt.figure(4)
+	
+	funcCount = len(solution)
+	plotCount = funcCount * 2;
+	x_result = [x for x, v in result]
+
+	for i in xrange(funcCount):
+		plt.subplot(plotCount, 1, i * 2 + 1)
+
+		if i == 0:
+			plt.title(u'Сравнение точного и численного решения: ')
+
+		exact_solution = [solution[i](x, 0) for x in x_result]
+		numeric_solution = [v[i] for x, v in result]
+
+		plt.ylabel(captions[i])
+		plt.plot(x_result, exact_solution, color="g")
+		plt.plot(x_result, numeric_solution, color="r")
+
+		plt.subplot(plotCount, 1, i * 2 + 2)
+
+		diff_solutions = [ abs(u - v) for u, v in itertools.izip(exact_solution, numeric_solution)]
+		plt.plot(x_result, diff_solutions, color="b")
+		plt.ylabel('|u - v|')
+		if i == funcCount - 1:
+			plt.xlabel(u'x, время: ')
+
+	g.show()
 
 x0 = 0.0
 w0 = 0.0
@@ -75,6 +172,7 @@ C1 = 0
 C2 = 0
 
 history = []
+result = [(xn, vn)]
 
 method = rk2
 p = 2
@@ -109,6 +207,7 @@ for i in xrange(1, Nmax + 1):
 	if eps / (2.0**(p+1)) <= S_max <= eps:
 		xn += hn
 		vn = vn_1
+		result.append((xn, vn))
 
 	elif eps < S_max:
 		hn = hn / 2.0
@@ -119,6 +218,7 @@ for i in xrange(1, Nmax + 1):
 		vn = vn_1
 		hn = hn * 2.0
 		C2 += 1
+		result.append((xn, vn))
 
 	#compare exact solution
 	un = v_func(sol, xn, 0)
@@ -139,15 +239,23 @@ for i in xrange(1, Nmax + 1):
 
 	# print xn, "\t", vn, 
 
-for h in history:
-	print 'i:', h['n'], "\t", 'x:', h['x'], '\t','h:',h['h'], '\t','C1:',h['C1'], '\t','C2:', h['C2']
-	print "       v:", h['v']
-	print "      v2:", h['v2']
-	print "       u:", h['u']
-	print "|v - v2|:", h['v1v2']
-	print "       S:", h['S']
-	print "    Smax:", h['Smax']
-	print " |u - v|:", h['uv']
+
+# for h in history:
+# 	print 'i:', h['n'], "\t", 'x:', h['x'], '\t','h:',h['h'], '\t','C1:',h['C1'], '\t','C2:', h['C2']
+# 	print "       v:", h['v']
+# 	print "      v2:", h['v2']
+# 	print "       u:", h['u']
+# 	print "|v - v2|:", h['v1v2']
+# 	print "       S:", h['S']
+# 	print "    Smax:", h['Smax']
+# 	print " |u - v|:", h['uv']
 	
 maxUv = tuple( max((h['uv'][i] for h in history)) for i in xrange(len(vn)) )
 print 'max |u - v|:', maxUv
+
+captions = ['phi', 'phi\'', 'w']
+show_S_plot(history)
+show_Func_plot(result, captions)
+show_Fase_portrait(result, captions, (1, 0))
+show_exact_and_test_solution(result, captions, sol)
+raw_input()
